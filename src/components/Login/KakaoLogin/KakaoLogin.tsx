a0e8b8ecce04a7c609ab71d6fa9d8e16d3409cc9
이형진 <114052845+Lee-Hyeong-Jin@users.noreply.github.com>
2023-08-15 23:04:44 +0900

    feature:auth with social and email final

--- src/components/Login/KakaoLogin/KakaoLogin.tsx
+++ src/components/Login/KakaoLogin/KakaoLogin.tsx
@@ -1,10 +1,87 @@
+import SocialKaKao from 'react-kakao-login';
+import {
+  useLoginKaKaoUserMutation,
+  useRegisterKaKaoUserMutation,
+} from 'services/authApi';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setUser } from 'features/auth/authSlice';
+import { useEffect } from 'react';
 import styles from './KakaoLogin.module.css';
 
-function KakaoLogin(): JSX.Element {
+interface UserData {
+  response: {
+    access_token: string;
+  };
+}
+
+type Variant = 'LOGIN' | 'REGISTER';
+
+interface Props {
+  variant: Variant;
+}
+
+function KakaoLogin({ variant }: Props): JSX.Element {
+  const kakaoClientId = '04e51adfeb37816173428908a9f46ab6';
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [
+    loginUser,
+    {
+      data: loginData,
+      isSuccess: isLoginSuccess,
+      isError: isLoginError,
+      error: loginError,
+    },
+  ] = useLoginKaKaoUserMutation();
+
+  const [
+    registerUser,
+    {
+      data: registerData,
+      isSuccess: isRegisterSuccess,
+      isError: isRegisterError,
+      error: registerError,
+    },
+  ] = useRegisterKaKaoUserMutation();
+
+  const kakaoOnSuccess = async (data: UserData) => {
+    if (variant === 'LOGIN') {
+      loginUser({ accessToken: data.response.access_token });
+    } else {
+      registerUser({
+        accessToken: data.response.access_token,
+        authorities: 'ROLE_USER',
+      });
+    } // 엑세스 토큰 백엔드로 전달
+  };
+  const kakaoOnFailure = (error: object) => {
+    console.log(error);
+  };
+
+  useEffect(() => {
+    if (isLoginSuccess) {
+      toast.success('user login');
+      dispatch(setUser({ tokens: loginData.tokens }));
+      navigate('/');
+    }
+
+    if (isRegisterSuccess) {
+      toast.success('register');
+      navigate('/login');
+    }
+  }, [isLoginSuccess, isRegisterSuccess]);
+
   return (
-    <button className={styles.btn} type="submit">
-      Kakao Login
-    </button>
+    <SocialKaKao
+      token={kakaoClientId}
+      onSuccess={kakaoOnSuccess}
+      onFail={kakaoOnFailure}
+      className={styles.btn}
+      style={{}}
+    />
   );
 }
 
--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -33,7 +33,50 @@ export const authApi = createApi({
         };
       },
     }),
+    loginKaKaoUser: builder.mutation({
+      query: (body: { accessToken: string }) => {
+        return {
+          url: '/user/social/kakao/login',
+          method: 'PATCH',
+          body,
+        };
+      },
+    }),
+    registerKaKaoUser: builder.mutation({
+      query: (body: { accessToken: string; authorities: string }) => {
+        return {
+          url: '/user/social/kakao/register',
+          method: 'POST',
+          body,
+        };
+      },
+    }),
+    loginGoogleUser: builder.mutation({
+      query: (body: { accessToken: string }) => {
+        return {
+          url: '/user/social/google/login',
+          method: 'PATCH',
+          body,
+        };
+      },
+    }),
+    registerGoogleUser: builder.mutation({
+      query: (body: { accessToken: string; authorities: string }) => {
+        return {
+          url: '/user/social/google/register',
+          method: 'POST',
+          body,
+        };
+      },
+    }),
   }),
 });
 
-export const { useLoginUserMutation, useRegisterUserMutation } = authApi;
+export const {
+  useLoginUserMutation,
+  useRegisterUserMutation,
+  useLoginKaKaoUserMutation,
+  useRegisterKaKaoUserMutation,
+  useLoginGoogleUserMutation,
+  useRegisterGoogleUserMutation,
+} = authApi;
