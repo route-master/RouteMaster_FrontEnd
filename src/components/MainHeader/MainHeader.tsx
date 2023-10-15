0cef129836a8ac894787110d886c51fabc99662a
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-10-16 02:46:55 +0900

    style: MainHeader

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -6,49 +6,39 @@ import styles from './MainHeader.module.css';
 function MainHeader(): JSX.Element {
   return (
     <header className={styles.container}>
-      <div className={styles.header_top}>
-        <div className={styles.logo_container}>
-          <div className={styles.img_container}>
-            <Link to="/">
-              <img src={LogoImage} alt="logo" className={styles.logo_img} />
-            </Link>
-          </div>
-          <div className={styles.header_name}>
-            <h1>Route Master</h1>
-          </div>
+      <div className={styles.logo_container}>
+        <div className={styles.img_container}>
+          <Link to="/">
+            <img src={LogoImage} alt="logo" className={styles.logo_img} />
+          </Link>
         </div>
-        <div className={styles.searchbar}>
-          <SearchBar />
-        </div>
-        <div className={styles.header_btn_container}>
-          <button type="button" className={styles.header_btn}>
-            <Link to="/plan-list">Trips</Link>
-          </button>
-          <button id="login-btn" type="button" className={styles.header_btn}>
-            <Link to="/login">Login</Link>
-          </button>
+        <div className={styles.header_name}>
+          <h1>Route Master</h1>
         </div>
       </div>
-
-      <div className={styles.header_bottom}>
-        <ul className={styles.nav}>
-          <li className={styles.nav_item}>
-            <button type="button" className={styles.nav_btn}>
-              <Link to="/attractions/stay">Hotels</Link>
-            </button>
-          </li>
-          <li className={styles.nav_item}>
-            <button type="button" className={styles.nav_btn}>
-              <Link to="/attractions/event">Activities</Link>
-            </button>
-          </li>
-          <li className={styles.nav_item}>
-            <button type="button" className={styles.nav_btn}>
-              <Link to="/attractions/restaurant">Restaurants</Link>
-            </button>
-          </li>
-        </ul>
+      <ul className={styles.nav}>
+        <li className={styles.nav_item}>
+          <button type="button" className={styles.nav_btn}>
+            <Link to="/attractions/stay">Hotels</Link>
+          </button>
+        </li>
+        <li className={styles.nav_item}>
+          <button type="button" className={styles.nav_btn}>
+            <Link to="/attractions/event">Activities</Link>
+          </button>
+        </li>
+        <li className={styles.nav_item}>
+          <button type="button" className={styles.nav_btn}>
+            <Link to="/attractions/restaurant">Restaurants</Link>
+          </button>
+        </li>
+      </ul>
+      <div className={styles.searchbar}>
+        <SearchBar />
       </div>
+      <button id="login-btn" type="button" className={styles.login_btn}>
+        <Link to="/login">Login</Link>
+      </button>
     </header>
   );
 }
