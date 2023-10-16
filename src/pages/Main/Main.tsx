02b78d7f26ad73618c2409903c19aa3cbfb7c82b
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-10-16 10:25:30 +0900

    feat: introduction in MainPage

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -27,9 +27,13 @@ function Main(): JSX.Element {
       <div className={styles.mainheader}>
         <MainHeader />
       </div>
-      <div className={styles.search_container}>
+      <div className={styles.planbox}>
+        <h1>여행 계획을 세워보세요!</h1>
+        여행을 갈 때마다 생기는 무수한 파일들이 지겹지 않나요? <br />
+        어디에 무슨 일정이 있는지 헷갈리셨나요? <br />
+        이제 RouteMaster와 함께 여행을 효과적으로 관리해보세요!
         <button type="button" className={styles.link_to_planlist}>
-          <Link to="/plan-list">Trips</Link>
+          <Link to="/plan-list">플랜 만들러 바로가기</Link>
         </button>
       </div>
       <div className={styles.recommend_container}>
