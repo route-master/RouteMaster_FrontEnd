11dae2bc8331fc0f1713d4fb4f27eb2598a9e569
Jeonghyeon Lee <66584938+JHyeon0915@users.noreply.github.com>
2023-10-16 14:45:53 +0900

    hotfix

--- src/store/Slices/attractions/thunks.tsx
+++ src/store/Slices/attractions/thunks.tsx
@@ -10,7 +10,7 @@ export const fetchAttractionsByType = createAsyncThunk(
     if (arg.type === 'stay') {
       requestURL = `/attraction/search/${arg.type}?numOfRows=${NumOfRows}&pageNo=${arg.page}&arrange=A&areaCode=1&sigunguCode=1`;
     } else if (arg.type === 'event') {
-      requestURL = `${process.env.REACT_APP_API_URL}/attraction/search/${arg.type}?numOfRows=${NumOfRows}&pageNo=${arg.page}&arrange=A&eventStartDate=20230701&areaCode=1&sigunguCode=1`;
+      requestURL = `http://api.route-master.org/attraction/search/${arg.type}?numOfRows=${NumOfRows}&pageNo=${arg.page}&arrange=A&eventStartDate=20230701&areaCode=1&sigunguCode=1`;
     } else {
       requestURL = `${process.env.REACT_APP_API_URL}/attraction/search/${arg.type}/keyword-based?numOfRows=${NumOfRows}&pageNo=${arg.page}&keyword=%EC%84%9C%EC%9A%B8`;
     }
