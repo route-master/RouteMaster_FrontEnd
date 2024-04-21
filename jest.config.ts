b5a26460f26fb2e63b89fd14ba8547884fd1b09e
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-04-21 18:10:35 +0900

    chore: edit jest.config.ts and install packages as needed

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,12 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'ts-node',
   verbose: true, // show all test results
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
 
 export default config;
